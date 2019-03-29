f44f44d23e50258445eadfc3ce2da6a779db6ca1
pschuller22 <pschuller22@gmail.com>
2019-03-29 16:39:18 -0400

    patsbranch

--- app/src/main/java/uncme/seniors_at_work/User.java
+++ app/src/main/java/uncme/seniors_at_work/User.java
@@ -9,7 +9,7 @@ public class User {
     String firstName = "Default";
     String lastName = "User";
     String email;
-    String test;
+    String test1;
 
     //constructors
     public User(String email) {
