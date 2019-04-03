667e3c4a0e7aad38a800812e9cad7adce21cc109
tylercobb7 <tcobb8@uncc.edu>
2019-04-03 14:03:38 -0400

    Began to add Code in SecondActivty to connect to firebase for up/downvotes

---
+++ app/src/main/java/VoteCounter.java
@@ -0,0 +1,68 @@
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
+
+import uncme.seniors_at_work.R;
+import uncme.seniors_at_work.SecondActivity;
+
+public class VoteCounter extends SecondActivity {
+    ImageButton sUpvoteButton;
+    ImageButton sDownvoteButton;
+    TextView sVoteCondition;
+
+    DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mConditionRef = myRef.child("Votes");
+    private static final String TAG = "SecondActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        sVoteCondition = (TextView)findViewById(R.id.voteCondition);
+        sUpvoteButton = (ImageButton)findViewById(R.id.upvoteButton);
+        sDownvoteButton = (ImageButton)findViewById(R.id.downvoteButton);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mConditionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            Integer upvoteCounter = dataSnapshot.getValue(Integer.class);
+            sVoteCondition.setText(upvoteCounter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        sUpvoteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mConditionRef.setValue("1");
+            }
+        });
+        sDownvoteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mConditionRef.setValue(-1);
+            }
+        });
+    }
+}
--- app/src/main/java/uncme/seniors_at_work/SecondActivity.java
+++ app/src/main/java/uncme/seniors_at_work/SecondActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.media.Image;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.ImageButton;
 import android.widget.VideoView;
 import android.widget.Button;
@@ -23,50 +25,25 @@ import java.net.URI;
 
 public class SecondActivity extends AppCompatActivity {
 
-    private static final String TAG = "SecondActivity";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
     }
+
     public void videoPlay(View v) {
 
         Intent intent = new Intent(SecondActivity.this, FullscreenVideo.class);
         startActivity(intent);
 
     }
-    public void userLogout(View v){
+
+    public void userLogout(View v) {
         Intent intent = new Intent(SecondActivity.this, MainActivity.class);
         startActivity(intent);
     }
-    public void basicReadWrite() {
-        // [START write_message]
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Hello, World!");
-        // [END write_message]
 
-        // [START read_message]
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
-            }
 
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });
-        // [END read_message]
-    }
 }
 
