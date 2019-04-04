4089fafacdc4dd4584f08b96807b82a9478c51bd
lai <ssaepha1@uncc.edu>
2019-04-04 04:19:55 -0400

    Code to check if all information has input then saves it into a hashmap and transfer them to an account created activity where they can click sign in to go back to beginning

---
+++ app/src/main/java/uncme/seniors_at_work/AccountCreated.java
@@ -0,0 +1,28 @@
+package uncme.seniors_at_work;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AccountCreated extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_created);
+
+        button = (Button)findViewById(R.id.btnBackToSignIn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AccountCreated.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/uncme/seniors_at_work/UserSignUp.java
+++ app/src/main/java/uncme/seniors_at_work/UserSignUp.java
@@ -1,22 +1,27 @@
 package uncme.seniors_at_work;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import com.google.firebase.database.core.Tag;
+import android.widget.Toast;
 
 import java.util.Calendar;
+import java.util.HashMap;
 
 
 public class UserSignUp extends AppCompatActivity {
 
+    private EditText UserName, UserEmail, UserPass, UserDOB;
+    private Button createAcc;
     private TextView displayDate;
     private DatePickerDialog.OnDateSetListener dateSetListener;
 
@@ -25,7 +30,19 @@ public class UserSignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_sign_up);
 
+        UserName = (EditText) findViewById(R.id.etUsername);
+        UserEmail = (EditText) findViewById(R.id.etUserEmail);
+        UserPass = (EditText) findViewById(R.id.etUserPass);
+        createAcc = (Button) findViewById(R.id.btnCreateAccount);
         displayDate = (TextView) findViewById(R.id.etUserDOB);
+
+        createAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SaveAccountInformation();
+            }
+        });
+
 
         //Date of Birth selector
         displayDate.setOnClickListener(new View.OnClickListener() {
@@ -55,5 +72,37 @@ public class UserSignUp extends AppCompatActivity {
                 displayDate.setText(date);
             }
         };
+    }
+
+    private void SaveAccountInformation() {
+
+        String username = UserName.getText().toString();
+        String useremail = UserEmail.getText().toString();
+        String userpass = UserPass.getText().toString();
+        String userdob = displayDate.getText().toString();
+
+
+        if(TextUtils.isEmpty(username)){
+            Toast.makeText(this, "Username field is empty", Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(useremail)){
+            Toast.makeText(this, "Email Address field is empty", Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(userpass)){
+            Toast.makeText(this, "Password field is empty", Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(userdob)){
+            Toast.makeText(this, "Date of Birth field is empty", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            HashMap userMap = new HashMap();
+            userMap.put("username", username);
+            userMap.put("useremail", useremail);
+            userMap.put("userpass", userpass);
+            userMap.put("userdob", userdob);
+
+            Intent intent = new Intent(UserSignUp.this, AccountCreated.class);
+            startActivity(intent);
+        }
     }
 }
