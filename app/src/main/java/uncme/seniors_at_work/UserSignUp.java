3b28cb83f4ca9f818758ccc1b895a32a42a3b26c
lai <ssaepha1@uncc.edu>
2019-04-04 03:33:59 -0400

    Date of Birth when clicked will open a date dialog to choose from

--- app/src/main/java/uncme/seniors_at_work/UserSignUp.java
+++ app/src/main/java/uncme/seniors_at_work/UserSignUp.java
@@ -1,13 +1,59 @@
 package uncme.seniors_at_work;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import com.google.firebase.database.core.Tag;
+
+import java.util.Calendar;
+
 
 public class UserSignUp extends AppCompatActivity {
+
+    private TextView displayDate;
+    private DatePickerDialog.OnDateSetListener dateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_sign_up);
+
+        displayDate = (TextView) findViewById(R.id.etUserDOB);
+
+        //Date of Birth selector
+        displayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        UserSignUp.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        dateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+
+                String date = month + "/" + day + "/" + year;
+                displayDate.setText(date);
+            }
+        };
     }
 }
