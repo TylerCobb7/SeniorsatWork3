28972f9bbdd15db4f7e93ac872dd2cb44892ade9
David Scanlon <dscanlon@uncc.edu>
2019-04-04 16:04:05 -0400

    fixed settings button

--- app/src/main/java/uncme/seniors_at_work/HomePage.java
+++ app/src/main/java/uncme/seniors_at_work/HomePage.java
@@ -62,6 +62,7 @@ public class HomePage extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(HomePage.this, EditAccountSettingsActivity.class);
                 intent.putExtra(SERIALIZE_DATA, user);
+                startActivity(intent);
             }
         });
 
