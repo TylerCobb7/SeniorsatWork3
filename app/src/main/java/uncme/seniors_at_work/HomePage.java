5a0917b9fcc9776336b6151aeeb64f5d2ae85930
lai <ssaepha1@uncc.edu>
2019-04-05 13:36:51 -0400

    Navigation on homepage and selectedvideo activities. Fixed some intent values when signing in.

--- app/src/main/java/uncme/seniors_at_work/HomePage.java
+++ app/src/main/java/uncme/seniors_at_work/HomePage.java
@@ -10,22 +10,32 @@ import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.ImageButton;
+import android.widget.Toast;
 import android.widget.VideoView;
 import android.widget.Button;
 import android.widget.MediaController;
+
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import java.net.URI;
+
+import uncme.seniors_at_work.AccountActivity.LoginActivity;
 
 
 public class HomePage extends AppCompatActivity {
@@ -38,6 +48,11 @@ public class HomePage extends AppCompatActivity {
     ImageButton settingsButton;
     int reddit;
     User user;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    RecyclerView postList;
+    Intent intent;
+    FirebaseAuth auth;
 
     DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
     DatabaseReference mConditionRef = myRef.child("VoteScore");
@@ -46,11 +61,14 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        sVoteCondition = (TextView)findViewById(R.id.voteCondition);
-        sUpvoteButton = (ImageButton)findViewById(R.id.upvoteButton);
-        sDownvoteButton = (ImageButton)findViewById(R.id.downvoteButton);
-        settingsButton = (ImageButton) findViewById(R.id.settings_button);
-        user  = (User) getIntent().getSerializableExtra("serializedata");
+        sVoteCondition = (TextView) findViewById(R.id.voteCondition);
+        sUpvoteButton = (ImageButton) findViewById(R.id.upvoteButton);
+        sDownvoteButton = (ImageButton) findViewById(R.id.downvoteButton);
+
+        user = (User) getIntent().getSerializableExtra("serializedata");
+
+        //get auth instance
+        auth = FirebaseAuth.getInstance();
 
         //get the user name text view and put the username of the user in it
         postUserName = findViewById(R.id.post_user_name);
@@ -58,20 +76,53 @@ public class HomePage extends AppCompatActivity {
 
         //functionality for account settings button
 
-        settingsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               Intent intent = new Intent(HomePage.this, EditAccountSettingsActivity.class);
-                intent.putExtra(SERIALIZE_DATA, user);
-                startActivity(intent);
-            }
-        });
 
+        //Navigation
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawable_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        View navView = navigationView.inflateHeaderView(R.layout.navigation_header);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                UserMenuSelector(item);
+                return false;
+            }
+        }); //End of Navigation
     }
 
+    private void UserMenuSelector(MenuItem item) {
 
+        switch(item.getItemId()){
+            case R.id.nav_profile:
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                break;
 
+            case R.id.nav_home:
+                intent = new Intent(HomePage.this, HomePage.class);
+                intent.putExtra(SERIALIZE_DATA, user);
+                startActivity(intent);
+                break;
 
+            case R.id.nav_settings:
+                intent = new Intent(HomePage.this, EditAccountSettingsActivity.class);
+                intent.putExtra(SERIALIZE_DATA, user);
+                startActivity(intent);
+                break;
+
+            case R.id.nav_Logout:
+                intent = new Intent(HomePage.this, LoginActivity.class);
+                intent.putExtra(SERIALIZE_DATA, user);
+                signOut();
+                startActivity(intent);
+                break;
+        }
+    }
+
+    public void signOut(){
+        auth.signOut();
+    }
     public void videoPlay(View v) {
 
         Intent intent = new Intent(HomePage.this, FullscreenVideo.class);
