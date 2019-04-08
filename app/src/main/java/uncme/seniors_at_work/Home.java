82f7e3fb9831e1c8337d5e9ee84b71e9fc57182e
lai <ssaepha1@uncc.edu>
2019-04-07 21:26:08 -0400

    adding border to post and setting image scale type to fitcenter

--- app/src/main/java/uncme/seniors_at_work/Home.java
+++ app/src/main/java/uncme/seniors_at_work/Home.java
@@ -99,7 +99,7 @@ public class Home extends AppCompatActivity {
         postList = (RecyclerView) findViewById(R.id.all_users_post_list);
         postList.setHasFixedSize(true);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setReverseLayout(false);
         linearLayoutManager.setStackFromEnd(true);
         postList.setLayoutManager(linearLayoutManager);
 
